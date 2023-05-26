b70761aae6e39fc52da1e439fbacbfb2f6c6353b
Harsh1992 <harsh.dusane@greatlearning.in>
2023-05-26 09:23:48 +0530

    Added CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 from app.service.product_service import Product
 from app.service.user_service import User
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
